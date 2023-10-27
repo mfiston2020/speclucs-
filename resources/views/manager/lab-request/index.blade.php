c8106450b4026f51b38e794125159c584dbd2cda
mfiston2020 <mfiston2020@gmail.com>
2023-10-27 11:13:05 +0200

    sdlfj

--- resources/views/manager/lab-request/index.blade.php
+++ resources/views/manager/lab-request/index.blade.php
@@ -946,9 +946,10 @@
                                                                     $availability = true;
                                                                     $description = null;
                                                                     $right_len = $request->unavailableproducts->where('eye', 'right')->first();
+                                                                    
                                                                     if (!$right_len) {
                                                                         $right_len = $request->soldproduct->where('eye', 'right')->first();
-                                                                        dd($right_len);
+
                                                                         $right_len = $powers->where('product_id',$right_len->product_id)->first();
                                                                         $availability = false;
                                                                     }
