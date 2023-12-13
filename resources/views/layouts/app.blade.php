066f1f223e352faceb4823c606ece1ae590c3cc4
mfiston2020 <mfiston2020@gmail.com>
2023-12-13 17:54:32 +0200

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
     <title>Stock</title>
     <!-- Custom CSS -->
     <link href="{{ asset('dashboard/assets/dist/css/style.min.css')}}" rel="stylesheet">
 
     @stack('css')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVVEZ32NLV"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HVVEZ32NLV');
+    </script>
 
 </head>
 
@@ -27,9 +36,9 @@
                 <div class="lds-pos"></div>
             </div>
         </div>
-        
+
         @yield('content')
-        
+
     </div>
     <!-- ============================================================== -->
     <!-- All Required js -->
